348bac7498883a4bbbb7dce4f9dd48832e6ab6f2
GoldGliders <yutashx@gmail.com>
2020-07-09 12:27:53 +0900

    FIX: fix the directory tree of api1

--- api/api/models/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-from flaskr import logger
-from pymongo import MongoClient
-
-client = MongoClient(os.environ["MONGO_URI"])
-db = client.LIBRARYBOT
--- api/api/views/index.py
+++
@@ -1,7 +0,0 @@
-from flaskr import logger
-from flaskr.api import bp
-
-
-@bp.route("/index", methods=["GET"])
-def index():
-    return "api bp"
