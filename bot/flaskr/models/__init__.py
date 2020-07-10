5ec1e53db36e7618803fc8744925109d923ca8de
GoldGliders <yutashx@gmail.com>
2020-07-10 09:55:50 +0900

    FIX: fix routing with path and subdomain

--- bot/flaskr/models/__init__.py
+++ bot/flaskr/models/__init__.py
@@ -1,3 +1,7 @@
+import os
 from flaskr.models.client import firestore_client
 
-db = firestore_client("instance/key.json")
+ROOTPATH = "/".join(os.getcwd().split("/")[:-2])
+ROOTPATH = ROOTPATH if ROOTPATH else "/app"
+
+db = firestore_client(ROOTPATH + "/instance/key.json")
