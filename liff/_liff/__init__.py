5e7406e295e7592af9b93078ad3225e538da2e6a
GoldGliders <yutashx@gmail.com>
2020-07-13 11:36:27 +0900

    REMOVE: remove _liff

--- liff/_liff/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-from flask import Blueprint, Flask, render_template
-from flaskr import logger
-
-bp = Blueprint("liff", __name__, static_folder="static", template_folder="template")
-
-from flaskr.liff.views import index
-logger.info("create liff bp")
