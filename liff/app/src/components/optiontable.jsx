1e96f94d1112e68a082c6e9e95c05bbfbde6e882
GoldGliders <yutashx@gmail.com>
2020-07-19 16:29:42 +0900

    IMPROVE: design option page

--- liff/app/src/components/optiontable.jsx
+++ liff/app/src/components/optiontable.jsx
@@ -3,6 +3,29 @@ import { initializeLiff } from "./liffInit"
 import liff from "@line/liff"
 import Checkbox from "@material-ui/core/Checkbox"
 import Button from "@material-ui/core/Button"
+import Card from "@material-ui/core/Card"
+import CardActions from "@material-ui/core/CardActions"
+import CardContent from "@material-ui/core/CardContent"
+import CardMedia from "@material-ui/core/CardMedia"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import { withStyles } from "@material-ui/core/styles"
+
+const useStyles = ((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  card: {
+    margin: 16,
+  },
+  button: {
+    marginTop: 16,
+    marginBottom: 32,
+  },
+  row: {
+    textAlign: "left",
+  },
+}))
 
 class OptionTable extends React.Component{
   constructor(props){
@@ -47,19 +70,25 @@ class OptionTable extends React.Component{
     }
   }
 
-  row(options, displayName, idToken){
+  row(options, displayName, idToken, classes){
     const keys = Object.keys(options)
     return (
       <div>
-        {
-          keys.map((key, num) => (
-            <div key={num}>
-              <Checkbox color="primary" className="selector" type="checkbox" defaultChecked={options[key]} name={key}/>
-              {displayName[key]}
-            </div>
-          ))
-        }
-        <Button variant="contained" onClick={() => this.sendOption(idToken)}>Save</Button>
+        <Grid container >
+          {
+            keys.map((key, num) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={num}>
+                <Grid >
+                  <Checkbox color="primary" className="selector" type="checkbox" defaultChecked={options[key]} name={key}/>
+                  {displayName[key]}
+                </Grid>
+              </Grid>
+            ))
+          }
+        </Grid>
+        <Grid container justify="center" className={classes.button} >
+          <Button variant="contained" onClick={() => this.sendOption(idToken)}>Save</Button>
+        </Grid>
       </div>
     )
   }
@@ -90,6 +119,7 @@ class OptionTable extends React.Component{
   }
 
   render(){
+    const {classes} = this.props
     if (this.state.error){
       liff.logout()
       return (
@@ -100,11 +130,14 @@ class OptionTable extends React.Component{
     }else{
       return (
         <div>
-          {this.row(this.state.options, this.state.displayName, this.state.idToken)}
+          <Card variant="outlined" className={classes.card}>
+            {this.row(this.state.options, this.state.displayName, this.state.idToken, classes)}
+          </Card>
         </div>
       )
     }
   }
 }
 
-export default OptionTable
+//export default OptionTable
+export default withStyles(useStyles, {withTheme: true})(OptionTable)
