82a7d812dd254cc9341b03866e1f15fa45441463
GoldGliders <yutashx@gmail.com>
2020-07-22 22:31:46 +0900

    FIX: fix location of key.json

--- api/flaskr/models/__init__.py
+++ api/flaskr/models/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flaskr.models.client import firestore_client
 
-ROOTPATH = "/".join(os.getcwd().split("/")[:-2])
+#ROOTPATH = "/".join(os.getcwd().split("/")[:-2])
+ROOTPATH = os.getcwd()
 ROOTPATH = ROOTPATH if ROOTPATH else "/app"
 db = firestore_client(ROOTPATH + "/instance/key.json")
--- bot/flaskr/models/__init__.py
+++ bot/flaskr/models/__init__.py
@@ -1,7 +1,8 @@
 import os
 from flaskr.models.client import firestore_client
 
-ROOTPATH = "/".join(os.getcwd().split("/")[:-2])
+# ROOTPATH = "/".join(os.getcwd().split("/")[:-2])
+ROOTPATH = os.getcwd()
 ROOTPATH = ROOTPATH if ROOTPATH else "/app"
 
 db = firestore_client(ROOTPATH + "/instance/key.json")
